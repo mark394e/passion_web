3353a77432a595220dd04031d50a56b67f573d3e
Markus <markuslund@live.dk>
2022-02-24 23:02:31 +0100

    sidste styling til allersidst

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function start() {
   console.log("start");
 
   // Definerer stien til json-array fra restdb i stedet for lokal .json-fil
+  // Henter 2 collections fra samme database
   const url = "https://passion-410f.restdb.io/rest/musikere";
   const url2 = "https://passion-410f.restdb.io/rest/omos";
   const key = "620e21bb34fd621565858704";
@@ -26,30 +27,33 @@ function start() {
   let artister;
   let filter = "alle";
 
+  // Lytter efter "klik" på menu-knap til burgermenu
   document.querySelector("#menuknap").addEventListener("click", toggleMenu);
 
+  // Toggler klassen "hidden" hver gang der klikkes på menu-knappen
   function toggleMenu() {
     console.log("toggleMenu");
     document.querySelector("#menu").classList.toggle("hidden");
 
     let erSkjult = document.querySelector("#menu").classList.contains("hidden");
 
+    // Ændre menu-knappens "udseende" alt efter om burgermenuen er skjult eller ej
     if (erSkjult == true) {
       document.querySelector("#menuknap").textContent = "☰";
     } else {
-      document.querySelector("#menuknap").textContent = "X";
+      document.querySelector("#menuknap").textContent = "x";
     }
   }
 
-  // sætter eventlistener på alle knapper i nav'en og lytter efter klik på knapperne
   const filterKnapper = document.querySelectorAll(".nav button");
-
   const filterKnapperBurger = document.querySelectorAll("#menu button");
 
+  //  sætter eventlistener på alle knapper i burgermenu og lytter efter klik på knapperne
   filterKnapperBurger.forEach((knap) =>
     knap.addEventListener("click", filtrerInfluencer)
   );
 
+  // sætter eventlistener på alle knapper i nav'en og lytter efter klik på knapperne
   filterKnapper.forEach((knap) =>
     knap.addEventListener("click", filtrerInfluencer)
   );
@@ -69,7 +73,7 @@ function start() {
     header.textContent = this.textContent;
   }
 
-  // Henter json-data fra restdb via fetch()
+  // Henter json-data fra restdb via fetch() fra to forskellige collections i samme database
   async function hentData() {
     const respons = await fetch(url, options);
     const respons2 = await fetch(url2, options);
@@ -86,7 +90,7 @@ function start() {
 
     section.textContent = ""; // Her resetter jeg DOM'en ved at tilføje en tom string
 
-    // for hver ret i arrayet, skal der tjekkes om de opfylder filter-kravet (forretter, hovedretter eller desserter) og derefter vises i DOM'en.
+    // for hver blogger i arrayet, skal der tjekkes om de opfylder filter-kravet og derefter vises i DOM'en.
     bloggere.forEach((blogger) => {
       if (filter == blogger.influencer) {
         document.querySelector(".tekstOmAlle p").textContent = "";
@@ -95,24 +99,26 @@ function start() {
           "img/" + blogger.billede2 + ".jpg";
         klon2.querySelector(".bloggerTekst").textContent = blogger.omos;
 
-        // tilføjer klon-template-elementet til main-elementet (så det hele vises i DOM'en)
+        // tilføjer klon-template-elementet til section-elementet (så det hele vises i DOM'en)
         section.appendChild(klon2);
-        // tilføjer section-elementet til body før main-elementet
+        // tilføjer section-elementet til min "header" der har klassen .splash
         document.querySelector(".splash").appendChild(section);
-      } else if (filter == "alle") {
+      }
+      // Hvis filteret er sat til alle, indsættes denne tekst
+      else if (filter == "alle") {
         document.querySelector(".tekstOmAlle p").textContent =
           "Fire af landets mest etablerede musikbloggere fra hovedstaden peger på deres 2022 favoritter. Se deres nøje udvalgte bands og artister, og læs hvorfor netop disse kunstnere kommer til at blinke på stjernehimlen i det nye år. Bliv inspireret af både etablerede og nye acts, som forhåbentligt leverer flere hits i 2022.";
       }
     });
   }
 
-  // loop'er gennem alle retter i json-arrayet
+  // loop'er gennem alle artister i json-arrayet
   function visArtister() {
     console.log("visArtister");
 
     main.textContent = ""; // Her resetter jeg DOM'en ved at tilføje en tom string
 
-    // for hver ret i arrayet, skal der tjekkes om de opfylder filter-kravet (forretter, hovedretter eller desserter) og derefter vises i DOM'en.
+    // for hver artist i arrayet, skal der tjekkes om de opfylder filter-kravet og derefter vises i DOM'en.
     artister.forEach((artist) => {
       if (filter == artist.influencer || filter == "alle") {
         const klon = template.cloneNode(true);
@@ -120,7 +126,7 @@ function start() {
         klon.querySelector(".navn").textContent = artist.navn;
         klon.querySelector(".korttekst").textContent = artist.kort_beskrivelse;
         klon.querySelector(".genre").textContent = "Genre: " + artist.genre;
-        // tilføjer eventlistner til hvert article-element og lytter efter klik på artiklerne
+        // tilføjer eventlistner til hvert article-element og lytter efter klik på artiklerne. Funktionen "visDetaljer" bliver kaldt ved klik.
         klon
           .querySelector("article")
           .addEventListener("click", () => visDetaljer(artist));
@@ -131,7 +137,7 @@ function start() {
     });
   }
 
-  // tilføjer tekst og billede, fra den valgte ret, til popup-vindue. Samt sætter cursor til default, så man ikke tror man kan klikke på elementet igen.
+  // tilføjer objekter fra arrayet (for hver artist) til popup-vindue. Samt sætter cursor til default, så man ikke tror man kan klikke på elementet igen.
   function visDetaljer(artist) {
     console.log(artist);
     document.querySelector(".nav").style.position = "inherit";
